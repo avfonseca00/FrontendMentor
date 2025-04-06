d4f0f51809fbec5931d5a2a63435702c84c626f6
avfonseca00 <avfonseca00@gmail.com>
2025-04-06 16:41:13 -0400

    fix(Root): Fix problems showing popup in mobile

--- article-preview-component-master/js/script.js
+++ article-preview-component-master/js/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {  
     const shareButton = document.querySelector('.share__button');  
     const popup = document.querySelector('.popup');  
+    const authorContainer = document.querySelector('.author__container');
 
     shareButton.addEventListener('click', function() {  
+        authorContainer.classList.toggle('visibility');
         popup.classList.toggle('visible');  
     });  
 });  
