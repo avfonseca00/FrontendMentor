8f6cb845d8cf10c76d94a62e9c6e1941a47f583b
avfonseca00 <avfonseca00@gmail.com>
2025-04-20 16:58:54 -0400

    feat(Root): Show feedback message to user on submit

--- interactive-rating-component-main/js/script.js
+++ interactive-rating-component-main/js/script.js
@@ -1,8 +1,15 @@
 document.getElementById("submitRating").onclick = function() {  
     const rating = document.querySelector('input[name="rating"]:checked');  
+    const thankContainer = document.getElementById('thanku__container')
+    const ratingContainer = document.querySelector(".rating__state__continer")
+    const badge = document.querySelector(".badge")
+
     if (rating) {  
-        alert("Thanks for your rating: " + rating.value);  
-        document.getElementById("ratingModal").style.display = "none"; // Cierra el modal  
+        const ratingValue = rating.value
+
+        badge.innerHTML = `You selected ${ratingValue} out of 5`
+        ratingContainer.style.display = "none"
+        thankContainer.style.display = "flex"
     } else {  
         alert("Please, choose a rating");  
     }  
